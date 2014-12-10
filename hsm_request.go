2ada7b31441fc56c4cbc36fef98ccd97f717f8d1
Robert Read <robert.read@intel.com>
2014-12-09 17:16:14 -0800

    golu: add ArchiveID to Archive action and use Fids instead of names
    Update the HSM request functions to take Fid slice instead
    of path names. Since we start with Fids and end with Fids, it
    makes no sends to send pathnames.

--- hsm_request.go
+++ hsm_request.go
@@ -9,39 +9,37 @@ import "C"
 
 import (
 	"fmt"
-	"path"
 	"reflect"
-	"strings"
 	"unsafe"
 )
 
-func RequestHsmArchive(fsID FilesystemID, archiveId uint, fileList []string) error {
-	return hsmRequest(fsID, C.HUA_ARCHIVE, archiveId, fileList)
+func RequestHsmArchive(fsID FilesystemID, archiveId uint, fids []Fid) error {
+	return hsmRequest(fsID, C.HUA_ARCHIVE, archiveId, fids)
 }
 
-func RequestHsmRestore(fsID FilesystemID, archiveId uint, fileList []string) error {
-	return hsmRequest(fsID, C.HUA_RESTORE, archiveId, fileList)
+func RequestHsmRestore(fsID FilesystemID, archiveId uint, fids []Fid) error {
+	return hsmRequest(fsID, C.HUA_RESTORE, archiveId, fids)
 }
 
-func RequestHsmRelease(fsID FilesystemID, archiveId uint, fileList []string) error {
-	return hsmRequest(fsID, C.HUA_RELEASE, archiveId, fileList)
+func RequestHsmRelease(fsID FilesystemID, archiveId uint, fids []Fid) error {
+	return hsmRequest(fsID, C.HUA_RELEASE, archiveId, fids)
 }
 
-func RequestHsmRemove(fsID FilesystemID, archiveId uint, fileList []string) error {
-	return hsmRequest(fsID, C.HUA_REMOVE, archiveId, fileList)
+func RequestHsmRemove(fsID FilesystemID, archiveId uint, fids []Fid) error {
+	return hsmRequest(fsID, C.HUA_REMOVE, archiveId, fids)
 }
 
-func RequestHsmCancel(fsID FilesystemID, archiveId uint, fileList []string) error {
-	return hsmRequest(fsID, C.HUA_CANCEL, archiveId, fileList)
+func RequestHsmCancel(fsID FilesystemID, archiveId uint, fids []Fid) error {
+	return hsmRequest(fsID, C.HUA_CANCEL, archiveId, fids)
 }
 
-func hsmRequest(fsID FilesystemID, cmd uint, archiveId uint, fileList []string) error {
+func hsmRequest(fsID FilesystemID, cmd uint, archiveId uint, fids []Fid) error {
 	mnt, err := fsID.Path()
 	if err != nil {
 		return err
 	}
 
-	if _, err = request(mnt, cmd, archiveId, fileList); err != nil {
+	if _, err = request(mnt, cmd, archiveId, fids); err != nil {
 		return err
 	}
 	return nil
@@ -49,17 +47,8 @@ func hsmRequest(fsID FilesystemID, cmd uint, archiveId uint, fileList []string) 
 
 // Request submits an HSM request for list of files
 // The max suported size of the fileList is about 50.
-func request(r string, cmd uint, archiveID uint, inList []string) (int, error) {
-	// Make a copy so that we don't modify the supplied list.
-	fileList := make([]string, len(inList))
-	copy(fileList, inList)
-	for idx, filePath := range fileList {
-		if !strings.HasPrefix(filePath, r) {
-			fileList[idx] = path.Join(r, filePath)
-		}
-	}
-
-	fileCount := len(fileList)
+func request(r string, cmd uint, archiveID uint, fids []Fid) (int, error) {
+	fileCount := len(fids)
 	if fileCount < 1 {
 		return 0, fmt.Errorf("Request must include at least 1 file!")
 	}
@@ -83,12 +72,7 @@ func request(r string, cmd uint, archiveID uint, inList []string) (int, error) {
 		Cap:  fileCount,
 	}
 	userItems := *(*[]C.struct_hsm_user_item)(unsafe.Pointer(&hdr))
-	for i, file := range fileList {
-		fid, err := LookupFid(file)
-		if err != nil {
-			fmt.Printf("%s: unable to lookup fid (%s)", file, err)
-			continue
-		}
+	for i, fid := range fids {
 		userItems[i].hui_extent.offset = 0
 		userItems[i].hui_extent.length = C.__u64(^uint(0))
 		userItems[i].hui_fid = C.lustre_fid(fid)
@@ -100,7 +84,7 @@ func request(r string, cmd uint, archiveID uint, inList []string) (int, error) {
 		return 0, fmt.Errorf("lustre: Can't submit incomplete request (%d/%d)", num, fileCount)
 	}
 
-	rc, err := C.llapi_hsm_request(C.CString(fileList[0]), hur)
+	rc, err := C.llapi_hsm_request(C.CString(r), hur)
 	if rc != 0 || err != nil {
 		if err != nil {
 			return 0, fmt.Errorf("lustre: Got error from llapi_hsm_request: %s", err.Error())
