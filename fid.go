75db12138590830e5dd60ed9b3e3750ac2b9757c
Michael MacDonald <michael.macdonald@intel.com>
2015-02-24 06:39:34 -0500

    Change lustre.Fid to an interface
    Defining Fid as an interface should make it easier to write tests
    and create mock Fid objects.

--- fid.go
+++ fid.go
@@ -12,48 +12,45 @@ import (
 	"path"
 )
 
-var (
-	// Special FID for .lustre
-	_DOT_LUSTRE_FID = Fid{0x200000002, 0x1, 0x0}
-)
-
 // Fid is a Lustre file identifier.
-type Fid C.lustre_fid
-
-func (fid Fid) String() string {
-	return fmt.Sprintf("[0x%x:0x%x:0x%x]",
-		fid.f_seq, fid.f_oid, fid.f_ver)
+type Fid interface {
+	String() string
+	IsZero() bool
+	IsDotLustre() bool
+	Pathnames(RootDir) ([]string, error)
+	AbsPathnames(RootDir) ([]string, error)
+	Path(RootDir) string
+	Open(RootDir) (*os.File, error)
+	OpenFile(RootDir, int, os.FileMode) (*os.File, error)
+	Stat(RootDir) (os.FileInfo, error)
+	Lstat(RootDir) (os.FileInfo, error)
+	MarshalJSON() ([]byte, error)
+	UnMarshalJSON([]byte) error
 }
 
-// IsZero is true of Fid is 0.
-func (fid Fid) IsZero() bool {
-	return fid.f_seq == 0 && fid.f_oid == 0 && fid.f_ver == 0
+type fid struct {
+	cfid *C.lustre_fid
 }
 
-// LookupFid returns the Fid for the given file or an error.
-func LookupFid(path string) (Fid, error) {
-	fid := Fid{}
-	rc, err := C.llapi_path2fid(C.CString(path), (*C.lustre_fid)(&fid))
-	if rc < 0 {
-		return Fid{}, fmt.Errorf("%s: fid not found (%s)", path, err.Error())
+// NewFid takes a *C.lustre_fid and returns a Go wrapper for it.
+func NewFid(cfid *C.lustre_fid) Fid {
+	return &fid{
+		cfid: cfid,
 	}
-	return fid, nil
 }
 
-// ParseFid converts a fid in string format to a Fid
-func ParseFid(fidstr string) (Fid, error) {
-	fid := Fid{}
-	if fidstr[0] == '[' {
-		fidstr = fidstr[1 : len(fidstr)-1]
-	}
-	n, err := fmt.Sscanf(fidstr, "0x%x:0x%x:0x%x", &fid.f_seq, &fid.f_oid, &fid.f_ver)
-	if err != nil {
-		return Fid{}, fmt.Errorf("lustre: unable to parse fid string: %v", fidstr)
-	}
-	if n != 3 {
-		return Fid{}, fmt.Errorf("lustre: unable to parse fid string: %v", fidstr)
-	}
-	return fid, nil
+func (f *fid) String() string {
+	return fmt.Sprintf("[0x%x:0x%x:0x%x]", f.cfid.f_seq, f.cfid.f_oid, f.cfid.f_ver)
+}
+
+// IsZero is true if Fid is 0.
+func (f *fid) IsZero() bool {
+	return f.cfid.f_seq == 0 && f.cfid.f_oid == 0 && f.cfid.f_ver == 0
+}
+
+// IsDotLustre is true if Fid is special .lustre entry.
+func (f *fid) IsDotLustre() bool {
+	return f.cfid.f_seq == 0x200000002 && f.cfid.f_oid == 0x1 && f.cfid.f_ver == 0x0
 }
 
 // Pathnames returns all paths for a FID.
@@ -61,8 +58,8 @@ func ParseFid(fidstr string) (Fid, error) {
 // This returns a slice containing all names that reference
 // the FID.
 //
-func (fid Fid) Pathnames(mnt RootDir) ([]string, error) {
-	return FidPathnames(mnt, fid.String())
+func (f *fid) Pathnames(mnt RootDir) ([]string, error) {
+	return FidPathnames(mnt, f.String())
 }
 
 // AbsPathnames returns all paths for a FID.
@@ -70,48 +67,72 @@ func (fid Fid) Pathnames(mnt RootDir) ([]string, error) {
 // This returns a slice containing all names that reference
 // the FID.
 //
-func (fid Fid) AbsPathnames(mnt RootDir) ([]string, error) {
-	return FidAbsPathnames(mnt, fid.String())
+func (f *fid) AbsPathnames(mnt RootDir) ([]string, error) {
+	return FidAbsPathnames(mnt, f.String())
 }
 
 // Path returns the "open by fid" path.
-func (fid Fid) Path(mnt RootDir) string {
-	return FidPath(mnt, fid.String())
+func (f *fid) Path(mnt RootDir) string {
+	return FidPath(mnt, f.String())
 }
 
 // Open by fid.
 // Returns readable file handle
-func (fid Fid) Open(mnt RootDir) (*os.File, error) {
-	return os.Open(fid.Path(mnt))
+func (f *fid) Open(mnt RootDir) (*os.File, error) {
+	return os.Open(f.Path(mnt))
 }
 
 // OpenFile by fid.
 // Returns readable file handle
-func (fid Fid) OpenFile(mnt RootDir, flags int, perm os.FileMode) (*os.File, error) {
-	return os.OpenFile(fid.Path(mnt), flags, perm)
+func (f *fid) OpenFile(mnt RootDir, flags int, perm os.FileMode) (*os.File, error) {
+	return os.OpenFile(f.Path(mnt), flags, perm)
 }
 
 // Stat by fid.
 // Returns readable file handle
-func (fid Fid) Stat(mnt RootDir) (os.FileInfo, error) {
-	return os.Stat(fid.Path(mnt))
+func (f *fid) Stat(mnt RootDir) (os.FileInfo, error) {
+	return os.Stat(f.Path(mnt))
 }
 
 // Lstat by fid.
 // Returns readable file handle
-func (fid Fid) Lstat(mnt RootDir) (os.FileInfo, error) {
-	return os.Lstat(fid.Path(mnt))
+func (f *fid) Lstat(mnt RootDir) (os.FileInfo, error) {
+	return os.Lstat(f.Path(mnt))
 }
 
 // MarshalJSON converts a Fid to a string for JSON.
-func (fid Fid) MarshalJSON() ([]byte, error) {
-	return []byte(`"` + fid.String() + `"`), nil
+func (f *fid) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + f.String() + `"`), nil
 }
 
 // UnMarshalJSON converts fid string to Fid.
-func (fid Fid) UnMarshalJSON(b []byte) (err error) {
-	fid, err = ParseFid(string(b))
+func (f *fid) UnMarshalJSON(b []byte) (err error) {
+	newFid, err := ParseFid(string(b))
+	f = newFid.(*fid)
 	return err
+}
+
+// LookupFid returns the Fid for the given file or an error.
+func LookupFid(path string) (Fid, error) {
+	cfid := C.lustre_fid{}
+	rc, err := C.llapi_path2fid(C.CString(path), (*C.lustre_fid)(&cfid))
+	if rc < 0 {
+		return &fid{}, fmt.Errorf("%s: fid not found (%s)", path, err.Error())
+	}
+	return NewFid(&cfid), nil
+}
+
+// ParseFid converts a fid in string format to a Fid
+func ParseFid(fidstr string) (Fid, error) {
+	cfid := C.lustre_fid{}
+	if fidstr[0] == '[' {
+		fidstr = fidstr[1 : len(fidstr)-1]
+	}
+	n, err := fmt.Sscanf(fidstr, "0x%x:0x%x:0x%x", &cfid.f_seq, &cfid.f_oid, &cfid.f_ver)
+	if err != nil || n != 3 {
+		return &fid{}, fmt.Errorf("lustre: unable to parse fid string: %v", fidstr)
+	}
+	return NewFid(&cfid), nil
 }
 
 // FidPathname returns a path for a FID.
