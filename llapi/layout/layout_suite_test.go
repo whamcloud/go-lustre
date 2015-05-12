8b4319bc02c4eb6b1d3a9957db1b513aff386dfd
Robert Read <robert.read@intel.com>
2015-05-11 21:24:17 -0700

    new package: llapi/layout
    This wraps most of liblustreapi_layout.c.  The underlying library
    is buggy and incomplete. We might need to change course and build
    this directly on top of the native lustre lov interface, so
    I've not covered everything yet.
    
    For a change I've also included a test suite, currently at 88.5%.

---
+++ llapi/layout/layout_suite_test.go
@@ -0,0 +1,30 @@
+package layout_test
+
+import (
+	"testing"
+
+	"github.intel.com/hpdd/test/harness"
+	"github.intel.com/hpdd/test/log"
+	"github.intel.com/hpdd/test/utils"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestLayout(t *testing.T) {
+	BeforeSuite(func() {
+		log.AddDebugLogger(&log.ClosingGinkgoWriter{GinkgoWriter})
+		if err := harness.Setup(); err != nil {
+			panic(err)
+		}
+	})
+
+	AfterSuite(func() {
+		if err := harness.Teardown(); err != nil {
+			panic(err)
+		}
+	})
+
+	RegisterFailHandler(Fail)
+	utils.RunSpecs(t, "Layout Suite")
+}
