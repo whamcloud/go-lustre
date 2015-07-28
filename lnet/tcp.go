479f82f5b4b15838940b17fdc2e27b91ca5a8239
Michael MacDonald <michael.macdonald@intel.com>
2015-07-28 12:21:13 -0400

    expose TcpNid.Address
    Normally the string representation of a Nid is enough, but sometimes
    it's useful to get at the underlying *net.IP.

--- lnet/tcp.go
+++ lnet/tcp.go
@@ -5,16 +5,20 @@ import (
 	"net"
 )
 
-type tcpNid struct {
-	ipAddress      net.IP
+type TcpNid struct {
+	IPAddress      *net.IP
 	driverInstance int
 }
 
-func (t *tcpNid) String() string {
-	return fmt.Sprintf("%s@%s%d", t.ipAddress, t.Driver(), t.driverInstance)
+func (t *TcpNid) String() string {
+	return fmt.Sprintf("%s@%s%d", t.IPAddress, t.Driver(), t.driverInstance)
 }
 
-func (t *tcpNid) Driver() string {
+func (t *TcpNid) Address() string {
+	return t.IPAddress.String()
+}
+
+func (t *TcpNid) Driver() string {
 	return "tcp"
 }
 
@@ -23,8 +27,8 @@ func newTcpNid(address string, driverInstance int) (Nid, error) {
 	if ip == nil {
 		return nil, fmt.Errorf("%q is not a valid IP address", address)
 	}
-	return &tcpNid{
-		ipAddress:      ip,
+	return &TcpNid{
+		IPAddress:      &ip,
 		driverInstance: driverInstance,
 	}, nil
 }
