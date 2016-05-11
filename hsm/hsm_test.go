cae49ff72915af9e850e8a285440549816b67713
Michael MacDonald <michael.macdonald@intel.com>
2016-05-11 17:14:04 -0400

    Update HSM test for new API
    Whoops, forgot to run the test to tell me that I broke the API...

--- hsm/hsm_test.go
+++ hsm/hsm_test.go
@@ -3,13 +3,19 @@ package hsm_test
 import (
 	"testing"
 
+	"golang.org/x/net/context"
+
 	"github.intel.com/hpdd/lustre/hsm"
 )
 
 // Basic test to ensure that the test API implements all interfaces
 func TestActionSource(t *testing.T) {
-	src := hsm.TestActionSource()
-	defer src.Stop()
+	src := hsm.NewTestSource()
+	ctx, stop := context.WithCancel(context.Background())
+	defer stop()
+	src.Start(ctx)
+
+	go src.GenerateRandomAction()
 
 	next := <-src.Actions()
 	handle, err := next.Begin(0, false)
